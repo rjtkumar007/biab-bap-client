ef604e9b77e139da5442143968c0bfb60a73af38
Kiran G <kirang@sahaj.ai>
2021-05-31 14:11:29 +0530

    Fix compile error

--- src/main/kotlin/org/beckn/one/sandbox/bap/SandboxBapConfig.kt
+++ src/main/kotlin/org/beckn/one/sandbox/bap/SandboxBapConfig.kt
@@ -1,11 +1,7 @@
 package org.beckn.one.sandbox.bap
 
 import com.mongodb.client.MongoDatabase
-import org.beckn.one.sandbox.bap.repositories.entities.Category
-import org.beckn.one.sandbox.bap.repositories.GenericCollection
-import org.beckn.one.sandbox.bap.repositories.entities.Item
 import org.litote.kmongo.KMongo
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,17 +12,11 @@ class SandboxBapConfig {
 
   @Bean
   @Profile(value = ["!test"])
-  fun database(@Value("\${database.mongo.url}") connectionString: String,
-               @Value("\${database.mongo.dbname}") databaseName: String): MongoDatabase {
+  fun database(
+    @Value("\${database.mongo.url}") connectionString: String,
+    @Value("\${database.mongo.dbname}") databaseName: String
+  ): MongoDatabase {
     val client = KMongo.createClient(connectionString)
     return client.getDatabase(databaseName)
   }
-
-  @Bean
-  fun categoryCollection(@Autowired database: MongoDatabase): GenericCollection<Category> =
-    GenericCollection.create(database)
-
-  @Bean
-  fun itemCollection(@Autowired database: MongoDatabase): GenericCollection<Item> =
-    GenericCollection.create(database)
 }
