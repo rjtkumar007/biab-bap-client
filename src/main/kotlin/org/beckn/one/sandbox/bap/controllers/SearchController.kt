52f5cb00aeac70d3e27fdab4530d2f02f403d3bc
Kiran G <kirang@sahaj.ai>
2021-06-16 15:11:41 +0530

    BO-34 | Add a mock search API

--- src/main/kotlin/org/beckn/one/sandbox/bap/controllers/SearchController.kt
+++ src/main/kotlin/org/beckn/one/sandbox/bap/controllers/SearchController.kt
@@ -1,6 +1,7 @@
 package org.beckn.one.sandbox.bap.controllers
 
 import org.beckn.one.sandbox.bap.dtos.Response
+import org.beckn.one.sandbox.bap.dtos.ResponseStatus
 import org.beckn.one.sandbox.bap.services.SearchService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class SearchController(@Autowired val searchService: SearchService) {
@@ -16,5 +18,11 @@ class SearchController(@Autowired val searchService: SearchService) {
   @ResponseBody
   fun searchV1(@RequestParam searchString: String): ResponseEntity<Response> {
     return searchService.search(searchString)
+  }
+
+  @RequestMapping("/v0/search")
+  @ResponseBody
+  fun searchV0(@RequestParam(required = false) searchString: String? = ""): ResponseEntity<Response> {
+    return ResponseEntity.ok(Response(status = ResponseStatus.ACK, message_id = UUID.randomUUID().toString()))
   }
 }
