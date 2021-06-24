cc303939a6fbc74b1532de8880df3c907ee9817b
Kiran G <kirang@sahaj.ai>
2021-06-24 15:13:50 +0530

    BO-43 | Make some fields optional nullable in schema

--- src/main/kotlin/org/beckn/one/sandbox/bap/schemas/ProviderWithItems.kt
+++ src/main/kotlin/org/beckn/one/sandbox/bap/schemas/ProviderWithItems.kt
@@ -58,12 +58,12 @@ data class ProtocolAddOn @Default constructor(
 data class ProtocolPrice @Default constructor(
   val currency: String,
   val value: String,
-  val estimatedValue: String,
-  val computedValue: String,
-  val listedValue: String,
-  val offeredValue: String,
-  val minimumValue: String,
-  val maximumValue: String
+  val estimatedValue: String?,
+  val computedValue: String?,
+  val listedValue: String?,
+  val offeredValue: String?,
+  val minimumValue: String?,
+  val maximumValue: String?
 )
 
 data class ProtocolConfigurableItemAttribute @Default constructor(
