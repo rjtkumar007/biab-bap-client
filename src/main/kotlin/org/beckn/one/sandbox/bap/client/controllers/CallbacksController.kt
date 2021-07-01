098b9afbf0db36196b1a550b50a270d86b74491d
Puneet Sharma <puneets@sahaj.ai>
2021-07-01 17:20:42 +0530

    BPWA-85 Temporary controller to fetch protocol responses

---
+++ src/main/kotlin/org/beckn/one/sandbox/bap/client/controllers/CallbacksController.kt
@@ -0,0 +1,69 @@
+package org.beckn.one.sandbox.bap.client.controllers
+
+import org.beckn.one.sandbox.bap.client.dtos.ClientErrorResponse
+import org.beckn.one.sandbox.bap.message.services.ResponseStorageService
+import org.beckn.one.sandbox.bap.schemas.ProtocolOnConfirm
+import org.beckn.one.sandbox.bap.schemas.ProtocolOnInit
+import org.beckn.one.sandbox.bap.schemas.ProtocolOnSelect
+import org.beckn.one.sandbox.bap.schemas.factories.ContextFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CallbacksController @Autowired constructor(
+  val initStore: ResponseStorageService<ProtocolOnInit>,
+  val selectStore: ResponseStorageService<ProtocolOnSelect>,
+  val confirmStore: ResponseStorageService<ProtocolOnConfirm>,
+  val contextFactory: ContextFactory
+) {
+
+  @RequestMapping("/client/v1/on_init")
+  @ResponseBody
+  fun onInitV1(
+    @RequestParam messageId: String
+  ) = initStore.findByMessageId(messageId)
+    .fold(
+      {
+        ResponseEntity
+          .status(it.status().value())
+          .body(ClientErrorResponse(context = contextFactory.create(), error = it.error()))
+      },
+      {
+        ResponseEntity.ok(it)
+      }
+    )
+
+  @RequestMapping("/client/v1/on_select")
+  @ResponseBody
+  fun onSelectV1(
+    @RequestParam messageId: String
+  ) = selectStore.findByMessageId(messageId).fold(
+    {
+      ResponseEntity
+        .status(it.status().value())
+        .body(ClientErrorResponse(context = contextFactory.create(), error = it.error()))
+    },
+    {
+      ResponseEntity.ok(it)
+    }
+  )
+
+  @RequestMapping("/client/v1/on_confirm")
+  @ResponseBody
+  fun onConfirmV1(
+    @RequestParam messageId: String
+  ) = confirmStore.findByMessageId(messageId).fold(
+    {
+      ResponseEntity
+        .status(it.status().value())
+        .body(ClientErrorResponse(context = contextFactory.create(), error = it.error()))
+    },
+    {
+      ResponseEntity.ok(it)
+    }
+  )
+}
