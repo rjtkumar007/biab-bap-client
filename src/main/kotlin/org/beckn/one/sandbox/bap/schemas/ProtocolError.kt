f1fa2bdd42fcab2966a29d3e152b822a2226ed94
Puneet Sharma <puneets@sahaj.ai>
2021-07-07 15:16:20 +0530

    BPWA-101 Updated Error schema class

--- src/main/kotlin/org/beckn/one/sandbox/bap/schemas/ProtocolError.kt
+++ src/main/kotlin/org/beckn/one/sandbox/bap/schemas/ProtocolError.kt
@@ -2,4 +2,18 @@ package org.beckn.one.sandbox.bap.schemas
 
 import org.beckn.one.sandbox.bap.Default
 
-data class ProtocolError @Default constructor(val code: String, val message: String)
+data class ProtocolError @Default constructor(
+  val code: String,
+  val message: String,
+  val type: Type? = null,
+  val path: String? = null,
+){
+
+  enum class Type(val value: String) {
+    CONTEXTERROR("CONTEXT-ERROR"),
+    COREERROR("CORE-ERROR"),
+    DOMAINERROR("DOMAIN-ERROR"),
+    POLICYERROR("POLICY-ERROR"),
+    JSONSCHEMAERROR("JSON-SCHEMA-ERROR");
+  }
+}
