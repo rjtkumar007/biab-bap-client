bdc190bdc75cec43e9e8acc2050aa1143dee20d4
Puneet Sharma <puneets@sahaj.ai>
2021-07-07 16:51:36 +0530

    BPWA-101 Added support for case insensitive enum parsing

--- src/main/kotlin/org/beckn/one/sandbox/bap/configurations/JacksonConfiguration.kt
+++ src/main/kotlin/org/beckn/one/sandbox/bap/configurations/JacksonConfiguration.kt
@@ -1,10 +1,7 @@
 package org.beckn.one.sandbox.bap.configurations
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.databind.util.StdDateFormat
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -19,6 +16,7 @@ class JacksonConfiguration {
     objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
     objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     objectMapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
+    objectMapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
     objectMapper.dateFormat = StdDateFormat().withColonInTimeZone(true)
     objectMapper.propertyNamingStrategy = PropertyNamingStrategies.SnakeCaseStrategy()
     return objectMapper
