777f2cc23603fc8a18a96313046de82ef74d6274
Puneet Sharma <puneets@sahaj.ai>
2021-07-15 18:19:01 +0530

    BPWA-80 Changed on_get_quote to a GET

--- src/main/kotlin/org/beckn/one/sandbox/bap/client/controllers/OnInitializeOrderController.kt
+++ src/main/kotlin/org/beckn/one/sandbox/bap/client/controllers/OnInitializeOrderController.kt
@@ -8,7 +8,10 @@ import org.beckn.one.sandbox.bap.schemas.factories.ContextFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class OnInitializeOrderController @Autowired constructor(
@@ -16,9 +19,9 @@ class OnInitializeOrderController @Autowired constructor(
   contextFactory: ContextFactory
 ) : BaseOnPollController<ProtocolOnInit, ClientInitResponse>(onPollService, contextFactory) {
 
-  @PostMapping("/client/v1/on_initialize_order")
+  @RequestMapping("/client/v1/on_initialize_order")
   @ResponseBody
   fun onInitializeOrderV1(
-    @RequestBody orderRequest: ProtocolOnInit
-  ): ResponseEntity<out ClientResponse> = onPoll(orderRequest.context.messageId)
+    @RequestParam messageId: String
+  ): ResponseEntity<out ClientResponse> = onPoll(messageId)
 }
