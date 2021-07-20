f21c8f1ebbb4aa6f02bd1a67bc727f6f8fe5209c
Puneet Sharma <puneets@sahaj.ai>
2021-07-20 18:22:36 +0530

    BPWA-108 Added maven repo which was remved by mistake

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,12 @@ allOpen {
 
 repositories {
   mavenCentral()
+  maven {
+    url = uri("s3://beckn-maven-artifacts/releases")
+    authentication {
+      val awsIm by registering(AwsImAuthentication::class)
+    }
+  }
   mavenLocal()
 }
 
