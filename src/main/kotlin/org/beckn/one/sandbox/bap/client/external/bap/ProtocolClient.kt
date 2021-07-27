72c8ce1631b5e3d31d80666150d0530c3230e868
Puneet Sharma <puneets@sahaj.ai>
2021-07-27 17:26:25 +0530

    BPWA-109 Fixed protocol client urls

--- src/main/kotlin/org/beckn/one/sandbox/bap/client/external/bap/ProtocolClient.kt
+++ src/main/kotlin/org/beckn/one/sandbox/bap/client/external/bap/ProtocolClient.kt
@@ -7,18 +7,18 @@ import retrofit2.http.Query
 
 
 interface ProtocolClient {
-  @GET("/protocol/v1/on_search")
+  @GET("protocol/v1/on_search")
   fun getSearchResponsesCall(@Query("messageId") messageId: String): Call<List<ProtocolOnSearch>>
 
-  @GET("/protocol/v1/on_select")
+  @GET("protocol/v1/on_select")
   fun getSelectResponsesCall(@Query("messageId") messageId: String): Call<List<ProtocolOnSelect>>
 
-  @GET("/protocol/v1/on_init")
+  @GET("protocol/v1/on_init")
   fun getInitResponsesCall(@Query("messageId") messageId: String): Call<List<ProtocolOnInit>>
 
-  @GET("/protocol/v1/on_confirm")
+  @GET("protocol/v1/on_confirm")
   fun getConfirmResponsesCall(@Query("messageId") messageId: String): Call<List<ProtocolOnConfirm>>
 
-  @GET("/protocol/v1/on_track")
+  @GET("protocol/v1/on_track")
   fun getTrackResponsesCall(@Query("messageId") messageId: String): Call<List<ProtocolOnTrack>>
 }
