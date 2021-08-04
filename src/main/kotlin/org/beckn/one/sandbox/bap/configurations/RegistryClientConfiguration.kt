24f206d53b41967e8860c437c311e41cde162f4c
Kiran G <kirang@sahaj.ai>
2021-08-04 17:10:18 +0530

    BPWA-112 | Implement retry for registry lookup requests

--- src/main/kotlin/org/beckn/one/sandbox/bap/configurations/RegistryClientConfiguration.kt
+++ src/main/kotlin/org/beckn/one/sandbox/bap/configurations/RegistryClientConfiguration.kt
@@ -1,31 +1,58 @@
 package org.beckn.one.sandbox.bap.configurations
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.resilience4j.core.IntervalFunction
+import io.github.resilience4j.retrofit.RetryCallAdapter
+import io.github.resilience4j.retry.Retry
+import io.github.resilience4j.retry.RetryConfig
 import org.beckn.one.sandbox.bap.client.external.registry.RegistryClient
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.server.WebServerException
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import org.springframework.http.HttpStatus
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
+import java.io.IOException
+import java.util.concurrent.TimeoutException
 
 
 @Configuration
 class RegistryClientConfiguration(
   @Autowired @Value("\${registry_service.url}")
   private val registryServiceUrl: String,
+  @Value("\${registry_service.retry.max_attempts}")
+  private val maxAttempts: Int,
+  @Value("\${registry_service.retry.initial_interval_in_millis}")
+  private val initialIntervalInMillis: Long,
+  @Value("\${registry_service.retry.interval_multiplier}")
+  private val intervalMultiplier: Double,
   @Autowired @Value("\${bpp_registry_service.url}")
   private val bppRegistryServiceUrl: String,
   @Autowired
   private val objectMapper: ObjectMapper
 ) {
+  private val retry: Retry = Retry.of("RegistryClient", RetryConfig.custom<Response<String>>()
+    .maxAttempts(maxAttempts)
+    .intervalFunction(
+      IntervalFunction
+        .ofExponentialBackoff(initialIntervalInMillis, intervalMultiplier)
+    )
+    .retryOnResult { response -> response.code() == HttpStatus.INTERNAL_SERVER_ERROR.value() }
+    .retryExceptions(IOException::class.java, TimeoutException::class.java, WebServerException::class.java)
+    .failAfterMaxAttempts(true)
+    .build())
+
   @Bean
   @Primary
   fun registryServiceClient(): RegistryClient {
     val retrofit = Retrofit.Builder()
       .baseUrl(registryServiceUrl)
       .addConverterFactory(JacksonConverterFactory.create(objectMapper))
+      .addCallAdapterFactory(RetryCallAdapter.of(retry))
       .build()
 
     return retrofit.create(RegistryClient::class.java)
---
+++ src/test/kotlin/org/beckn/one/sandbox/bap/client/shared/services/RegistryServiceRetrySpec.kt
@@ -0,0 +1,96 @@
+package org.beckn.one.sandbox.bap.client.shared.services
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED
+import io.kotest.assertions.arrow.either.shouldBeLeft
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import org.beckn.one.sandbox.bap.client.external.gateway.GatewayClient
+import org.beckn.one.sandbox.bap.client.shared.errors.gateway.GatewaySearchError
+import org.beckn.one.sandbox.bap.client.shared.errors.registry.RegistryLookupError
+import org.beckn.one.sandbox.bap.common.factories.MockNetwork
+import org.beckn.one.sandbox.bap.schemas.factories.UuidFactory
+import org.junit.jupiter.api.Assertions
+import org.mockito.Mockito.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
+import java.time.Clock
+import java.time.Instant
+import java.time.ZoneId
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles(value = ["test"])
+@TestPropertySource(locations = ["/application-test.yml"])
+class RegistryServiceRetrySpec @Autowired constructor(
+  val registryService: RegistryService,
+  val objectMapper: ObjectMapper,
+) : DescribeSpec() {
+  private val uuidFactory = mock(UuidFactory::class.java)
+  private val gatewayServiceClient: GatewayClient = mock(GatewayClient::class.java)
+
+  init {
+    describe("Search") {
+      MockNetwork.startAllSubscribers()
+      `when`(uuidFactory.create()).thenReturn("9056ea1b-275d-4799-b0c8-25ae74b6bf51")
+      val allGateways = MockNetwork.getAllGateways()
+
+      beforeEach {
+        MockNetwork.resetAllSubscribers()
+        reset(gatewayServiceClient)
+      }
+
+      it("should retry search call if api returns error") {
+        stubRegistryLookupApi(response = serverError())
+        stubRegistryLookupApi(forState = "Success", response = okJson(toJson(allGateways)))
+
+        val response = registryService.lookupGateways()
+
+        response
+          .fold(
+            { Assertions.fail("Lookup should have been retried but it wasn't. Response: $it") },
+            { it shouldBe allGateways }
+          )
+        MockNetwork.registry.verify(2, postRequestedFor(urlEqualTo("/lookup")))
+      }
+
+      it("should fail after max retry attempts") {
+        stubRegistryLookupApi(response = serverError(), forState = STARTED, nextState = "Failure")
+        stubRegistryLookupApi(response = serverError(), forState = "Failure", nextState = "Failure")
+
+        val response = registryService.lookupGateways()
+
+        response shouldBeLeft RegistryLookupError.Internal
+        verifyGatewaySearchApiIsInvoked(3)
+      }
+    }
+  }
+
+  private fun verifyGatewaySearchApiIsInvoked(numberOfTimes: Int = 1) {
+    MockNetwork.registry.verify(numberOfTimes, postRequestedFor(urlEqualTo("/lookup")))
+  }
+
+  private fun stubRegistryLookupApi(
+    forState: String = STARTED,
+    nextState: String = "Success",
+    response: ResponseDefinitionBuilder?
+  ) {
+    MockNetwork.registry
+      .stubFor(
+        post("/lookup")
+          .inScenario("Retry Scenario")
+          .whenScenarioStateIs(forState)
+          .willReturn(response)
+          .willSetStateTo(nextState)
+      )
+  }
+
+  private fun toJson(instance: Any) =
+    objectMapper.writeValueAsString(instance)
+
+}
