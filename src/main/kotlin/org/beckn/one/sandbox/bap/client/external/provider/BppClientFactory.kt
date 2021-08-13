9c26cb3493e39c3167e47e76dbe612ab7c1fa2f7
Kiran G <kirang@sahaj.ai>
2021-08-13 14:40:04 +0530

    BPWA-137 | Configure 10s timeout in BPP HTTP client

--- src/main/kotlin/org/beckn/one/sandbox/bap/client/external/provider/BppClientFactory.kt
+++ src/main/kotlin/org/beckn/one/sandbox/bap/client/external/provider/BppClientFactory.kt
@@ -14,6 +14,7 @@ import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
+import java.util.concurrent.TimeUnit
 
 @Service
 class BppClientFactory @Autowired constructor(
@@ -25,25 +26,42 @@ class BppClientFactory @Autowired constructor(
   @Value("\${bpp_service.retry.interval_multiplier}")
   private val intervalMultiplier: Double,
   @Value("\${beckn.security.enabled}") val enableSecurity: Boolean,
-  private val interceptor: SignRequestInterceptor
-) {
+  private val interceptor: SignRequestInterceptor,
+  @Value("\${bpp_service.timeouts.connection_in_seconds}") private val connectionTimeoutInSeconds: Long,
+  @Value("\${bpp_service.timeouts.read_in_seconds}") private val readTimeoutInSeconds: Long,
+  @Value("\${bpp_service.timeouts.write_in_seconds}") private val writeTimeoutInSeconds: Long,
+
+  ) {
   @Cacheable("bppClients")
   fun getClient(bppUri: String): BppClient {
-    val retry: Retry = RetryFactory.create(
+    val url = if (bppUri.endsWith("/")) bppUri else "$bppUri/"
+    val retrofit = Retrofit.Builder()
+      .baseUrl(url)
+      .client(buildHttpClient())
+      .addConverterFactory(JacksonConverterFactory.create(objectMapper))
+      .addCallAdapterFactory(RetryCallAdapter.of(getRetryConfig(bppUri)))
+      .addCallAdapterFactory(CircuitBreakerCallAdapter.of(CircuitBreakerFactory.create(bppUri)))
+      .build()
+    return retrofit.create(BppClient::class.java)
+  }
+
+  private fun buildHttpClient(): OkHttpClient {
+    val httpClientBuilder = OkHttpClient.Builder()
+      .connectTimeout(connectionTimeoutInSeconds, TimeUnit.SECONDS)
+      .readTimeout(readTimeoutInSeconds, TimeUnit.SECONDS)
+      .writeTimeout(writeTimeoutInSeconds, TimeUnit.SECONDS)
+    if (enableSecurity) {
+      httpClientBuilder.addInterceptor(interceptor)
+    }
+    return httpClientBuilder.build()
+  }
+
+  private fun getRetryConfig(bppUri: String): Retry {
+    return RetryFactory.create(
       bppUri,
       maxAttempts,
       initialIntervalInMillis,
       intervalMultiplier
     )
-    val okHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
-    val circuitBreaker = CircuitBreakerFactory.create(bppUri)
-    val url = if (bppUri.endsWith("/")) bppUri else "$bppUri/"
-    val retrofitBuilder = Retrofit.Builder()
-      .baseUrl(url)
-      .addConverterFactory(JacksonConverterFactory.create(objectMapper))
-      .addCallAdapterFactory(RetryCallAdapter.of(retry))
-      .addCallAdapterFactory(CircuitBreakerCallAdapter.of(circuitBreaker))
-    val retrofit = if (enableSecurity) retrofitBuilder.client(okHttpClient).build() else retrofitBuilder.build()
-    return retrofit.create(BppClient::class.java)
   }
 }
